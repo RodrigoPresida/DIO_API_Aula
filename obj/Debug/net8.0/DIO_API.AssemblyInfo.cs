825e22127fbfc2a4b56be17deef6ca347c971b3d
Rodrigo Presida <rodrigopresida@gmail.com>
2024-01-01 06:06:42 -0300

    DIO_API

--- obj/Debug/net8.0/DIO_API.AssemblyInfo.cs
+++ obj/Debug/net8.0/DIO_API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DIO_API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0306039616e8410b99b9811311644a0dbbabf282")]
 [assembly: System.Reflection.AssemblyProductAttribute("DIO_API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DIO_API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
